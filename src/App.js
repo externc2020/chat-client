a75c2bf7e5365b38ecfb4acd20556374055a2bea
杨阳 <yangyang179@meituan.com>
2021-04-30 18:03:40 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState([])
+  const [connected, setConnected] = useState(false)
+  const [nickname, setNickname] = useState("alice")
+  const ws = useRef(null)
+
+  useEffect(() => {
+    // ws.current = new WebSocket("ws://localhost:8080/ws")
+    ws.current = new WebSocket("wss://e9b6e8fa172e.ngrok.io/ws")
+    ws.current.onopen = () => {
+      console.log("open")
+      setConnected(true)
+    }
+    ws.current.onmessage = (e) => {
+      console.log(e.data)
+      setMessages(prev => [...prev, e.data])
+    }
+    ws.current.onclose = () => {
+      console.log("close")
+      setConnected(false)
+    }
+    ws.current.onerror = (e) => {
+      console.error("error", e)
+    }
+    return () => ws.current.close()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {connected && <div>Chat server connected!</div>}
+      <TextField label="Nickname" value={nickname} onChange={(e) => setNickname(e.target.value)}/>
+      <div>
+        {messages.map((m, i) => (<div key={i}>{m}</div>))}
+      </div>
+      <div>
+        
+        <TextField value={message} onChange={(e) => setMessage(e.target.value)}/>
+        <Button onClick={() => {
+          if (nickname && message) {
+            let m = nickname + ': ' + message
+            setMessages(prev => [...prev, m])
+            ws.current.send(m)
+            setMessage("")
+          }
+        }}>Send</Button>
+      </div>
     </div>
   );
 }
---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/ws',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:8080',
+      changeOrigin: true,
+      ws: true,
+      secure: false,
+      logLevel: 'debug',
+    })
+  );
+};
